77a04733b82b02bcd0c5c361f7509b70854f888c
renduiyang <3072085859@qq.com>
2021-03-16 19:46:28 +0800

    修正

---
+++ project4.0/cmake-build-debug/CMakeFiles/project4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project4.dir/main.cpp.obj"
+  "project4.exe"
+  "project4.exe.manifest"
+  "project4.lib"
+  "project4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
